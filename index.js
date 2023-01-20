3325731a3d49e335f92fa26e964df9df95a3128c
dhershman1 <dustinh17@gmail.com>
2023-01-20 17:58:56 -0500

    Added: Experimental step test

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
 import K from 'kyanite'
-import alea from './_internal/alea.js'
+import generateSeed, { alea } from './_internal/alea.js'
 import { _curry2, _curry3 } from './_internal/curry.js'
+
+function step (fnGen, seed) {
+  const aSeed = alea(seed)
+
+  return fnGen.step(aSeed)
+}
 
 /**
  * Generates a random number that will be no more than the provided maximum
  * @param {Number} max The maximum the number can be
- * @param {String} seed A seed provided by other functions to keep data consistent
+ * @param {String} seed The seed to generate the data
  * @returns {Number} A random number
  */
 function num (max, seed) {
-  const nextSeed = alea(seed).quick()
+  const nextSeed = generateSeed(seed).quick()
 
   return [Math.floor(nextSeed * max), nextSeed]
+}
+
+function int (min, max) {
+  return {
+    value: max,
+    step (seed) {
+      const nextSeed = seed.next()
+
+      return [int(min, Math.floor(nextSeed * max)).value, nextSeed]
+    }
+  }
+}
+
+/**
+ * Generates a random float style number that will stay below the provided maximum
+ * @param {Number} max The maximum the number can be
+ * @param {String|Number} seed The seed to generate the data
+ * @returns {Number} A random float style number
+ */
+function float (max, seed) {
+  const nextSeed = generateSeed(seed).quick()
+
+  return [nextSeed * max, nextSeed]
 }
 
 /**
  * Generates a random number between a min and max limit
  * @param {Number} min The minimum the number can be
  * @param {Number} max The maximum the number can be
- * @param {String} seed A seed provided by other functions to keep data consistent
+ * @param {String} seed The seed to generate the data
  * @returns {Number} A random number between the min and max provided
  */
 function minNum (min, max, seed) {
-  const nextSeed = alea(seed).quick()
+  const nextSeed = generateSeed(seed).quick()
 
   return [Math.floor(nextSeed * (max - min + 1)) + min, nextSeed]
 }
@@ -30,7 +59,7 @@ function minNum (min, max, seed) {
 /**
  * Pulls out a random value from a provided array
  * @param {Any[]} items An array of data items
- * @param {String} seed A seed provided by other functions to keep data consistent
+ * @param {String} seed The seed to generate the data
  * @returns {Any} A random value picked from the provided array
  */
 const pick = _curry2(function pick (items, seed) {
@@ -45,7 +74,7 @@ const pick = _curry2(function pick (items, seed) {
 /**
  * Takes an array of random based functions which expect a seed, and pipes through them properly to transform data
  * @param {Function[]} fns An array of random based functions (functions using seeds) to pipe through
- * @param {String} seed A seed provided by other functions to keep data consistent
+ * @param {String} seed The seed to generate the data
  * @returns {Any} The final product of the pipe transformation
  */
 function seeder (fns, seed) {
@@ -60,11 +89,11 @@ function seeder (fns, seed) {
 
 /**
  * Generate a random letter from A-Z
- * @param {String} seed A seed provided by other functions to keep data consistent
+ * @param {String} seed The seed to generate the data
  * @returns {String} A letter from A-Z
  */
 function letter (seed) {
-  const nextSeed = alea(seed).quick()
+  const nextSeed = generateSeed(seed).quick()
   const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
   return pick(letters, nextSeed)
@@ -100,30 +129,50 @@ function pieces (keys, tables, seed) {
 /**
  * Uses basic probability to pull data based on given weights
  * @param {Any[]} list An array of data values from a table
- * @param {String} seed A seed provided by other functions to keep data consistent
+ * @param {String} seed The seed to generate the data
  * @returns {Any} A random value from the given list
  */
 function probability (list, seed) {
-  let n = alea(seed).quick()
+  let n = generateSeed(seed).quick()
 
   for (const [item, prob] of list) {
     n = n - prob
 
     if (n < 0) {
-      return [item, alea(n).quick()]
+      return [item, generateSeed(n).quick()]
     }
   }
+}
+
+/**
+ * Performs a uniform probability pick against an Array
+ * @param {Any[]} list A list of values to for a uniform probability picker around
+ * @param {String|Number} seed The seed to generate the data
+ * @returns {[Any, Number]} A new array with the picked value as the first index and a new seed as the second
+ */
+function uniform ([a = '', ...list], seed) {
+  if (K.isEmpty(list)) {
+    return [a, generateSeed(seed).quick()]
+  }
+
+  const full = [a, ...list]
+  const weight = 100 / full.length
+
+  return probability(
+    K.map(v => [v, weight], full),
+    seed
+  )
 }
 
 /**
  * Creates a random date string based on the given format
  * @param {String} format The date format we are expecting
  * @param {Object} param1 The date table object holding months, days, and years
- * @param {String} seed A seed provided by other functions to keep data consistent
+ * @param {String} seed The seed to generate the data
  * @returns {String} A random date string formatted based on desired layout
  */
 function date (format, { months, days, years }, seed) {
-  const dateSeed = alea(seed).quick()
+  const dateSeed = generateSeed(seed).quick()
   const [[month, day, year], nextSeed] = seeder([
     pick(months),
     pick(days),
@@ -141,12 +190,16 @@ function date (format, { months, days, years }, seed) {
 }
 
 export default {
+  float: _curry2(float),
   num: _curry2(num),
   minNum: _curry3(minNum),
   seeder: _curry2(seeder),
   probability: _curry2(probability),
   date: _curry3(date),
   pieces: _curry3(pieces),
+  int: _curry2(int),
+  step,
   pick,
-  letter
+  letter,
+  uniform
 }
--- tests/random.spec.js
+++ tests/random.spec.js
@@ -1,67 +1,83 @@
 import test from 'tape'
 import random from '../index.js'
 
-test('random.num()', t => {
-  t.same(random.num(20, 'abc123'), [17, 0.8986478650476784])
-  t.same(random.num(20, 'abc'), [19, 0.9722709273919463])
-  t.end()
-})
+// test('random.num()', t => {
+//   t.same(random.num(20, 'abc123'), [17, 0.8986478650476784])
+//   t.same(random.num(20, 'abc'), [19, 0.9722709273919463])
+//   t.end()
+// })
 
-test('random.minNum()', t => {
-  t.same(random.minNum(1, 100, 'abc123'), [90, 0.8986478650476784])
-  t.same(random.minNum(1, 100, 'abc'), [98, 0.9722709273919463])
-  t.same(random.minNum(8, 10, 'abc123'), [10, 0.8986478650476784])
+// test('random.float()', t => {
+//   t.same(random.float(2, 'abc123'), [1.7972957300953567, 0.8986478650476784])
+//   t.same(random.float(2, 'foobar'), [1.6971524031832814, 0.8485762015916407])
+//   t.same(random.float(3, 'abc123'), [2.695943595143035, 0.8986478650476784])
 
-  t.end()
-})
+//   t.end()
+// })
 
-test('random.pick()', t => {
-  t.same(random.pick('abc123', 'ahhhh'), ['3', 0.8598122051917017])
-  t.same(random.pick([1, 2, 3], 'abc'), [3, 0.9722709273919463])
-  t.end()
-})
+// test('random.minNum()', t => {
+//   t.same(random.minNum(1, 100, 'abc123'), [90, 0.8986478650476784])
+//   t.same(random.minNum(1, 100, 'abc'), [98, 0.9722709273919463])
+//   t.same(random.minNum(8, 10, 'abc123'), [10, 0.8986478650476784])
 
-test('random.letter()', t => {
-  t.same(random.letter('abc123'), ['Z', 0.9875701994169503])
-  t.same(random.letter(0.9875701994169503), ['M', 0.49460635893046856])
-  t.end()
-})
+//   t.end()
+// })
 
-test('random.pieces()', t => {
-  t.same(
-    random.pieces(['line', 'zip'], { line: ['hello', 'world', 'zoooop'], zip: [44444, 55555, 66666] }, 'abc123'), ['zoooop 66666', 0.9875701994169503]
-  )
-  t.end()
-})
+// test('random.pick()', t => {
+//   t.same(random.pick('abc123', 'ahhhh'), ['3', 0.8598122051917017])
+//   t.same(random.pick([1, 2, 3], 'abc'), [3, 0.9722709273919463])
+//   t.end()
+// })
 
-test('random.seeder', t => {
-  t.same(random.seeder([
-    random.pick([1, 2, 3, 4]),
-    random.pick([1, 2, 3, 4]),
-    random.pick([1, 2, 3, 4]),
-    random.pick([1, 2, 3, 4]),
-    random.pick([1, 2, 3, 4])
-  ], 'wooopie'), [[3, 1, 1, 4, 2], 0.30550888809375465])
+// test('random.letter()', t => {
+//   t.same(random.letter('abc123'), ['Z', 0.9875701994169503])
+//   t.same(random.letter(0.9875701994169503), ['M', 0.49460635893046856])
+//   t.end()
+// })
 
-  t.end()
-})
+// test('random.pieces()', t => {
+//   t.same(
+//     random.pieces(['line', 'zip'], { line: ['hello', 'world', 'zoooop'], zip: [44444, 55555, 66666] }, 'abc123'), ['zoooop 66666', 0.9875701994169503]
+//   )
+//   t.end()
+// })
 
-test('random.probability()', t => {
-  t.same(random.probability([
-    ['hello', 0.5],
-    ['world', 0.5]
-  ], 'abc123'), ['world', 0.6556187274400145])
+// test('random.seeder', t => {
+//   t.same(random.seeder([
+//     random.pick([1, 2, 3, 4]),
+//     random.pick([1, 2, 3, 4]),
+//     random.pick([1, 2, 3, 4]),
+//     random.pick([1, 2, 3, 4]),
+//     random.pick([1, 2, 3, 4])
+//   ], 'wooopie'), [[3, 1, 1, 4, 2], 0.30550888809375465])
 
-  t.same(random.probability([
-    ['hello', 0.9],
-    ['world', 0.1]
-  ], 'abc123'), ['hello', 0.2178527475334704])
-  t.end()
-})
+//   t.end()
+// })
 
-test('random.date()', t => {
-  const datesTable = { months: ['01', '02', '03'], days: ['12', '09', '20'], years: ['2010', '2023', '2020'] }
-  t.same(random.date('M/D/Y', datesTable, 'abc123'), ['03/12/2023', 0.49460635893046856])
-  t.same(random.date('M/D/Y', datesTable, 'coolbeans'), ['03/09/2020', 0.9407318972516805])
+// test('random.probability()', t => {
+//   t.same(random.probability([
+//     ['hello', 0.5],
+//     ['world', 0.5]
+//   ], 'abc123'), ['world', 0.6556187274400145])
+
+//   t.same(random.probability([
+//     ['hello', 0.9],
+//     ['world', 0.1]
+//   ], 'abc123'), ['hello', 0.2178527475334704])
+//   t.end()
+// })
+
+// test('random.date()', t => {
+//   const datesTable = { months: ['01', '02', '03'], days: ['12', '09', '20'], years: ['2010', '2023', '2020'] }
+//   t.same(random.date('M/D/Y', datesTable, 'abc123'), ['03/12/2023', 0.49460635893046856])
+//   t.same(random.date('M/D/Y', datesTable, 'coolbeans'), ['03/09/2020', 0.9407318972516805])
+//   t.end()
+// })
+
+test('random.int()', t => {
+  const iter = random.int(1, 100)
+  console.log(iter)
+
+  console.log(random.step(iter, 'abc123'))
   t.end()
 })
