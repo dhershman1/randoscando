fb8d485d66f334b9a50054eca897bd3bfc48773f
dhershman1 <dustinh17@gmail.com>
2023-08-18 16:05:55 -0400

    Fixed: Cleaned up unit tests

--- tests/number/uniform.spec.js
+++ tests/number/uniform.spec.js
@@ -6,10 +6,10 @@ import uniform from '../../src/number/uniform.js'
 test('uniform()', t => {
   const data = range(1, 1000)
 
-  t.same(step(uniform([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 'abc123'), [8, 0.8987810940016061])
+  t.same(step(uniform([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 'abc123'), [9, 0.8987810940016061])
   t.same(step(uniform(data), 'abc123'), [898, 0.8987810940016061])
-  t.same(step(uniform(data), 'waaaa'), [219, 0.21161161735653877])
-  t.same(step(uniform(data), 'supercoolseed'), [830, 0.8298104661516845])
+  t.same(step(uniform(data), 'waaaa'), [212, 0.21161161735653877])
+  t.same(step(uniform(data), 'supercoolseed'), [829, 0.8298104661516845])
 
   t.end()
 })
